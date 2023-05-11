ab82ec93514237fc0e6fe1c8a0b1eb1808ececb9
gabrielmacaubas <gabrielmacaubas@outlook.com>
2023-05-11 11:56:55 -0300

    dia 03

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from galeria.models import Fotografia, Categoria
 
-# Register your models here.
+
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda', 'publicada')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_filter = ('categoria',)
+    list_editable = ('publicada',)
+    list_per_page = 10
+
+
+class ListandoCategorias(admin.ModelAdmin):
+    list_display = ('id', 'categoria')
+    list_display_links = ('id', 'categoria')
+    search_fields = ('categoria',)
+
+admin.site.register(Fotografia, ListandoFotografias)
+admin.site.register(Categoria, ListandoCategorias)
+
