d4917edeb3a2556088d358cb1d051bea65c4d563
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2023-05-15 11:24:54 -0300

    dia 05 commit

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,15 +1,123 @@
 from django import forms
+from django.contrib.auth.models import User, Group
 
 class LoginForms(forms.Form):
     nome_login = forms.CharField(
         label='Nome de Login',
         required=True,
-        max_length=100
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: João Silva"
+            }
+        )
     )
     senha = forms.CharField(
         label='Senha',
         required=True,
         max_length=70,
-        widget=forms.PasswordInput()
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha"
+            }
+        )
     )
+
+
+class CadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label='Nome de Cadastro',
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: João Silva"
+            }
+        )
+    )
+    email = forms.EmailField(
+        label='Email',
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: joaosilva@xpto.com"
+            }
+        )
+    )
+    senha_1 = forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha"
+            }
+        )
+    )
+    senha_2 = forms.CharField(
+        label='Confirme sua senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha novamente"
+            }
+        )
+    )
+
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        senha_1 = cleaned_data.get('senha_1')
+        senha_2 = cleaned_data.get('senha_2')
+
+        if senha_1 != senha_2:
+            raise forms.ValidationError('As senhas não são iguais!')
+        
+        return cleaned_data
+
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Email já cadastrado!')
+
+        return email
     
+
+    def clean_nome_cadastro(self):
+        nome = self.cleaned_data.get('nome_cadastro')
+
+        if User.objects.filter(username=nome).exists():
+            raise forms.ValidationError('Nome de usuário já cadastrado!')
+
+
+    def save(self, commit=True):
+        nome = self.cleaned_data.get('nome_cadastro')
+        email = self.cleaned_data.get('email')
+        senha = self.cleaned_data.get('senha_1')
+        usuario = User.objects.create_user(
+            username=nome,
+            email=email,
+            password=senha
+        )
+        
+        if commit:
+            usuario.save()
+
+            grupo = Group.objects.filter(pk=1).first()
+
+            if grupo:
+                usuario.groups.add(grupo)
+                usuario.save()
+            
+        return usuario
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
-from usuarios.forms import LoginForms
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, Group
+from usuarios.forms import LoginForms, CadastroForms
+
 
 def login(request):
     form = LoginForms()
+
     return render(request, 'usuarios/login.html', {"form": form})
 
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    form = CadastroForms()
+
+    if request.method == 'POST':
+        form = CadastroForms(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('login')
+
+    return render(request, 'usuarios/cadastro.html', {"form": form})
