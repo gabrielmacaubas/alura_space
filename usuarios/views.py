05258b28038c2ee653434779acf0aa870c482e90
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2023-05-16 12:53:12 -0300

    Update views.py

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, Group
+from django.contrib import auth
 from usuarios.forms import LoginForms, CadastroForms
 
 
 def login(request):
     form = LoginForms()
+    
+    if request.method == 'POST':
+        form = LoginForms(request.POST)
+
+        if form.is_valid():
+            nome = form['nome_login'].value()
+            senha = form['senha'].value()
+
+        usuario = auth.authenticate(
+            request,
+            username=nome,
+            password=senha
+        )
+        
+        if usuario is not None:
+            auth.login(request, usuario)
+
+            return redirect('index')
+        
+        else:
+            return redirect('login')
 
     return render(request, 'usuarios/login.html', {"form": form})
 
